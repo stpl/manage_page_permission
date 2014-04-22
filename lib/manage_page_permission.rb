e118066ad6cf4d898213f41b70ca9829c7a2709c
Nikhil D <ndashore@isystango.com>
2014-04-22 15:47:54 +0530

    moved wiki_controller_patch inside module

--- lib/manage_page_permission.rb
+++ lib/manage_page_permission.rb
@@ -1,5 +1,5 @@
 module ManagePagePermission
   def self.apply_patch
-    WikiController.send(:include, WikiControllerPatch)
+    WikiController.send(:include, ManagePagePermission::WikiControllerPatch)
   end
 end
